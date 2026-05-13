03c2a1620ddc4b009ab28192f287b1918214ba56
dmbots616 <deadmanbots616@gmail.com>
2026-05-13 19:37:07 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 
-# Show Python version to confirm it's 3.10
-python3.10 --version
+# Show Python version to confirm it's 3.11
+python3.11 --version
 
 # Upgrade pip and install dependencies
 pip install --upgrade pip
 pip install --no-cache-dir -r requirements.txt
 
-# Run the bot explicitly with Python 3.10
-python3.10 main.py
+# Run the bot explicitly with Python 3.11
+python3.11 main.py
